407ae3c1e29e33ee9913215067a91eb2ff9d177f
EmanuelCastelo <jescbf@yahoo.com.br>
2021-01-01 12:04:53 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -33,6 +33,12 @@ public class Program {
 				department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: Seller update ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update Completed!");
 
 	}
 
